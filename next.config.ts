60770ad26fe7b00c715c4b938f97cad68bf45363
greglim81 <limjunqu@gmail.com>
2025-05-22 20:54:13 +0800

    added to next.config,

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-// next.config.js
+// next.config.ts
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
-      'firebasestorage.googleapis.com'
+      'firebasestorage.googleapis.com',
+      'storage.googleapis.com',
+      'replicate.delivery'
     ],
   },
 };
